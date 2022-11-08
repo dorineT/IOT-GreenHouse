39ba37add27c0e5d898900fab2ce3bf75663c4a3
PierreJacobs <pierre.jacobs@student.unamur.be>
2022-11-08 09:54:18 +0100

    added fake sensors

--- Backend_src/app/manager/sensors/phidgets.py
+++ Backend_src/app/manager/sensors/phidgets.py
@@ -1,6 +1,8 @@
 from enum import Enum
 from typing import Callable
 
+from perlin_noise import PerlinNoise
+
 from Phidget22.Phidget import Phidget
 from Phidget22.Devices.LightSensor import LightSensor
 from Phidget22.Devices.TemperatureSensor import TemperatureSensor
@@ -13,6 +15,8 @@ class SensorType(Enum):
     LIGHT = 'light'
     TEMP = 'temperature'
     HUM = 'humidity'
+    CO2 = 'co2'
+    PH = 'ph'
 
 
 class Sensor:
@@ -87,3 +91,44 @@ class cHumSensor(Sensor):
 
     def value(self, max_delay: int) -> float:
         return self._reach(self._sensor.getHumidity, max_delay)
+
+class cCO2Sensor(Sensor):
+    
+    def __init__(self, serial_number: int, port: int) -> None:
+        noise = PerlinNoise(octaves=10, seed=1)
+        self.mean = 650
+        self.noise_map = [self.mean + self.mean*noise(i/100) for i in range(100)]
+        print(self.noise_map)
+        self.cidx = -1
+        self.cdir = 1
+        
+    def value(self, max_delay: int) -> float:
+        
+        if self.cidx == len(self.noise_map):
+            self.cdir = -1
+        elif self.cidx == -1:
+            self.cdir = 1
+        
+        self.cidx += self.cdir
+        return self.noise_map[self.cidx]
+        
+class cPHSensor(Sensor):
+    
+    def __init__(self, serial_number: int, port: int) -> None:
+        noise = PerlinNoise(octaves=10, seed=2)
+        self.mean = 7
+        self.noise_map = [self.mean + self.mean*0.2*noise(i/100) for i in range(100)]
+        print(self.noise_map)
+        self.cidx = -1
+        self.cdir = 1
+        
+    def value(self, max_delay: int) -> float:
+        
+        if self.cidx == len(self.noise_map):
+            self.cdir = -1
+        elif self.cidx == -1:
+            self.cdir = 1
+        
+        self.cidx += self.cdir
+        return self.noise_map[self.cidx]
+ 
