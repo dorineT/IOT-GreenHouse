f1996a1be0423d054083bffa6612e15b57c5902f
Dorine <dorine.thielemans@hotmail.fr>
2022-11-15 19:09:39 +0100

    house squelette information

--- CompagnonApp_src/ReactApp/my-app/src/screen/Home.js
+++ CompagnonApp_src/ReactApp/my-app/src/screen/Home.js
@@ -1,29 +1,45 @@
-import * as React from 'react';
-import { Text, View, Image, Box, Heading, VStack, HStack, Center } from 'native-base';
-import serreImg from '../../assets/serrepng.png'
-import { FontAwesome5 } from '@expo/vector-icons'; 
+import * as React from "react";
+import {
+  Text,
+  View,
+  Image,
+  Box,
+  Heading,
+  VStack,
+  HStack,
+  Center,
+} from "native-base";
+import serreImg from "../../assets/serrepng.png";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-export function HomeScreen({navigation}) {
+export function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Box bg={{
-        linearGradient: {
-          colors: ['lightBlue.300', 'blue.500'],
-          start: [0, 0],
-          end: [1, 0]
-        }
-      }} p="5" rounded={10}>
-      <HStack space={20} justifyContent="center">
+    <View >
+      <Heading p={3}>Les nouvelles !</Heading>
+      <Center>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["lightBlue.300", "blue.500"],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+        p="5"
+        rounded={5}
+        width={350}
+      >
+        <HStack space={20} justifyContent="center">
           <FontAwesome5 name="faucet" size={50} color="white" />
 
           <VStack space={5}>
-            <Heading color="white">Arrossage</Heading>  
+            <Heading color="white">Arrossage</Heading>
             <Text>le x du x à 10h</Text>
-          </VStack>            
+          </VStack>
         </HStack>
-      </Box>
-      <Text>Home!</Text>
+      </Box>    
       <Image source={serreImg} alt="serre" size="2xl" />
+      </Center>        
     </View>
   );
 }
--- CompagnonApp_src/ReactApp/my-app/src/screen/House.js
+++ CompagnonApp_src/ReactApp/my-app/src/screen/House.js
@@ -1,10 +1,105 @@
-import * as React from 'react';
-import { Text, View } from 'native-base';
+import React,{useState, useEffect} from "react";
+import { Text,Box, HStack, VStack, Heading, Fab, Center, ScrollView } from "native-base";
+import { Ionicons } from '@expo/vector-icons';
 
-export function HouseScreen({navigation}) {
+export function HouseScreen({ navigation }) {
+  const [luminosite, setLuminosite] = useState(null);
+
+  useEffect(() => {
+    //get data from api
+    setLuminosite(1000)
+   }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Informations de votre serre !</Text>
-    </View>
+    <ScrollView>     
+      <Fab  style={{backgroundColor: 'white'}} mb={10}
+       icon={<Ionicons name="ios-reload"  size={25} color="gray" /> }
+       onPress={() => {console.log('coucou')}} />
+      
+      <Center mt={10}>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["yellow.300", "warning.300"],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+        p="5" rounded={5} width={350} mb={5}
+      >
+        <HStack space={20} justifyContent="center">
+          <Ionicons name="ios-thermometer-outline" size={50} color="white" />
+          <VStack space={5}>
+            <Heading color="white">Température</Heading>
+            <Text>25 °C</Text>
+          </VStack>
+        </HStack>
+      </Box>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["green.400", "green.100"],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+        p="5" rounded={5} width={350} mb={5}
+      >
+        <HStack space={20} justifyContent="center">
+          {
+            luminosite === 0 ?
+            <Ionicons name="ios-thermometer-outline" size={50} color="white" />
+            :
+            <Ionicons name="ios-sunny" size={50} color="white" />
+          }
+        
+
+          <VStack space={5}>
+            <Heading color="white">Luminosité</Heading>
+            <Text>Ensoleillé</Text>
+          </VStack>
+        </HStack>
+      </Box>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["lightBlue.300", "blue.500"],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+        p="5" rounded={5} width={350} mb={5}
+      >
+        <HStack space={20} justifyContent="center">
+          <Ionicons name="ios-water" size={50} color="white" />
+
+          <VStack space={5}>
+            <Heading color="white">Humidité</Heading>
+            <Text>50 %</Text>
+          </VStack>
+        </HStack>
+      </Box>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["warning.900", "warmGray.100"],
+            start: [0, 0],
+            end: [1, 0],
+          },
+        }}
+        p="5" rounded={5} width={350} mb={5}
+      >
+        <HStack space={20} justifyContent="center">
+          <Ionicons name="ios-filter" size={50} color="white" />
+
+          <VStack space={5}>
+            <Heading color="white">Ph</Heading>
+            <Text>7</Text>
+            <Text>+ image</Text>
+          </VStack>
+        </HStack>
+      </Box>
+      </Center>
+    </ScrollView>
   );
 }
