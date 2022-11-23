00679e6704e51ffe729b74bfa150325ca2124412
Dorine <dorine.thielemans@hotmail.fr>
2022-11-23 20:19:16 +0100

    splash img + fix depency vulnerability + wifi inf

--- CompagnonApp_src/ReactApp/my-app/src/screen/Setting.js
+++ CompagnonApp_src/ReactApp/my-app/src/screen/Setting.js
@@ -1,10 +1,34 @@
-import * as React from 'react';
-import { Text, View } from 'native-base';
+import React, {useEffect} from 'react';
+import { Button, Text, View } from 'native-base';
+import NetInfo from '@react-native-community/netinfo';
+import * as Location from 'expo-location'
 
 export function SettingsScreen({navigation}) {
+  
+  useEffect(() => {
+      (async () => {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.log('Permission to access location was denied');
+          return;
+        }
+        const location = await Location.getCurrentPositionAsync({});
+        console.log('Location permission granted', location);
+      })();
+    }, []);
+
+  function wifi(){
+    NetInfo.fetch().then(state => {
+      console.log('Connection type', state.type);
+      console.log('Is connected?', state.isConnected);
+      console.log('Is connected?', state.details.ssid);
+    });
+  }
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Configuration pour accéder à votre serre</Text>
+      <Button onPress={ () => wifi()} > WIFI informations</Button>
     </View>
   );
 }
